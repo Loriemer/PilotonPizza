4a27ba6690c5495207237da327fbc55b6d3ee70e
asus <asus@LAPTOP-5RR1SHUE>
2023-10-23 00:52:56 +0800

    Add project files.

---
+++ PilotonPizza/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PilotonPizza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Diameter = null;
+            int Slices;
+            const double SliceSize = 14.13;
+            double Radius;
+            double Area;
+
+            Console.WriteLine("Enter diameter of pizza");
+            Diameter = Console.ReadLine();
+            double Diameter1 = Convert.ToDouble(Diameter);
+
+            Radius = Diameter1 / 2;
+            Area = Math.PI * Math.Pow(Radius, 2);
+            Slices = (int)(Area / SliceSize);
+
+            Console.WriteLine("A Diameter\" pizza will yield {0:no} slices", Slices);
+
+            Console.WriteLine("\n\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
